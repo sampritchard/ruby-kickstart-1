ce0f770a1f6152d3d04e688b2e4a2aaac06c7ccf
Sam Pritchard <sampritchard@msn.com>
2017-04-19 10:15:45 +0100

    1_input_output.rb

--- session2/challenge/1_input_output.rb
+++ session2/challenge/1_input_output.rb
@@ -3,10 +3,14 @@
 
 # Write a program that reads in two integers typed on the keybaord
 # and outputs their sum, difference, and product
-# 
+#
 # Standard input will be like "9 2\n" and will expect you to print
 # "11\n7\n18\n" to standard output.
 
 def sum_difference_product
-  # your code goes here
+  string_number = gets
+  integer = string_number.split.map {|n| n.to_i}
+  puts integer[0] + integer[1]
+  puts integer[0] - integer[1]
+  puts integer[0] * integer[1]
 end
