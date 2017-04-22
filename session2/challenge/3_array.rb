37fef7b5433c2cf213b48254972e66eb45994359
Sam Pritchard <sampritchard@msn.com>
2017-04-22 11:18:25 +0100

    3_array.rb

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -7,5 +7,10 @@
 
 class String
   def every_other_char
-  end
+  to_return = "" #was tring this as an array [] rather than a string? This is creating an empty string for the new one to go in
+  #"each_char" is paassing each character from a string to the given block. "with_index" iterates over all indexes in the block.
+  #In the block character and index are the parameters passed in and when on an even index, the character is passed to the new string to_return
+  each_char.with_index {|character, index| to_return << character if index.even?} 
+  return to_return #don't need to put "return"
+end
 end
