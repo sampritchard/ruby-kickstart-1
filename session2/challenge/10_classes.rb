8844b9dd97a9ddbe6637c1efad5c48f9b29d9943
Sam Pritchard <sampritchard@msn.com>
2017-04-23 18:07:39 +0100

    10_classes.rb

--- session2/challenge/10_classes.rb
+++ session2/challenge/10_classes.rb
@@ -16,4 +16,16 @@
 #
 
 class Person
+  #setting and getting name and age rather than going through the longer process
+  attr_accessor :name, :age
+
+  def initialize(name, age) #initialzize the name and age ojbects
+    @name = name
+    @age = age
+  end
+
+  def birthday #make a birthday method where age increasese with birthday
+    @age += 1
+  end
+
 end
