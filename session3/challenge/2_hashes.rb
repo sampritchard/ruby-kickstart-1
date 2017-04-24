7803428400b5551d742bbe2767e00ca07baa54be
Sam Pritchard <sampritchard@msn.com>
2017-04-24 13:49:10 +0100

    2_hashes.rb

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -8,3 +8,15 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
+def staircase(n)
+  to_return = {} #this will return a hash
+
+    1.upto n do |size| #starting at 1 in the array and going upto n; iterate over 'size'
+    #(1..n).to_a do |size|
+    next if size.even? #next if is skipping past the odd numbers in the array
+      all = Array.new(size) {|i| i + 1} #creating a new array called all where elements are iterated over and added
+      evens = all.select {|i| i.even?} #selects all even values in the new array called 'all'
+      to_return[size] = evens 
+    end
+   to_return
+end
