8873827ccb6e59fc78aafccc761da6efacb05a54
Sam Pritchard <sampritchard@msn.com>
2017-04-24 18:09:52 +0100

    16_hash.rb

--- session3/challenge/16_hash.rb
+++ session3/challenge/16_hash.rb
@@ -15,3 +15,10 @@
 # head = {:data => 6, :next => head}
 # print_list head                    # >> "6\n5\n4\n3\n2\n1\n"
 
+def print_list(list)
+  while list
+    puts list[:data]
+  #else
+    list = list[:next]
+  end
+end
