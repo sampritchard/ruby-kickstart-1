588e3c125720a125dca7a2594c82f46317a134ab
Sam Pritchard <sampritchard@msn.com>
2017-04-24 15:09:49 +0100

    6_arguments.rb

--- session3/challenge/6_arguments.rb
+++ session3/challenge/6_arguments.rb
@@ -17,3 +17,14 @@
 # match_maker true,  true,  true, true, nil     # => [false, true]
 # match_maker true,  true,  true, 0, nil        # => [false, true]
 
+def match_maker(*elements)
+to_return = [] #returned array for results
+deleted = elements.shift #removes the first element which is given and this determines what their value should be as per instructions
+#elements first element is now removed to deleted
+#.each_slice(2) is saying to iterate the given block of 2 elements
+#in the block, we have the two elements a and b
+#the double bang returns a boolean false for false or nil or true for everything else
+#"(false == false) == ("true") would push true to to_return because false == false is true!
+elements.each_slice(2){ |a,b| to_return << ((!!a == !!b) == !deleted)}
+to_return
+end
