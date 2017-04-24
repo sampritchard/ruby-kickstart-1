d8e096a922863cbe39654396f1c14479a104e872
Sam Pritchard <sampritchard@msn.com>
2017-04-24 14:10:08 +0100

    5_blocks.rb

--- session3/challenge/5_blocks.rb
+++ session3/challenge/5_blocks.rb
@@ -16,3 +16,12 @@
 #   order << i
 # end
 # order # => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+
+def spiral_access(two_d, &block)
+my_arr = [] #create an array for finished array to be stored in
+  two_d.each {|array| array.each {|num| my_arr << num}} #iterate over the 2d square array
+  #in each array in two_d, push the number to the newly created array
+  my_arr.sort!
+  my_arr.each {|i| block.call i} #call the block on each element in my_arr
+
+end
